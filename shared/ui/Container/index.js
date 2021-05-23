7aeb873bfd269a8aee9af6f7f7c2f362d789d598
Peter Pezaris <pez@codestream.com>
2021-05-22 22:14:51 -0400

    new terms of service acceptance

--- shared/ui/Container/index.js
+++ shared/ui/Container/index.js
@@ -20,9 +20,11 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, createTheme } from "../src/themes";
 import { closeAllPanels } from "../store/context/actions";
 import { WebviewErrorRequestType } from "@codestream/protocols/agent";
+import { PresentTOS } from "../Authentication/PresentTOS";
 
 const mapStateToProps = state => {
 	const team = state.teams[state.context.currentTeamId];
+
 	return {
 		bootstrapped: state.bootstrapped,
 		connectivityError: state.connectivity.error,
@@ -34,7 +36,8 @@ const mapStateToProps = state => {
 		ide: state.ide && state.ide.name ? state.ide.name : undefined,
 		serverUrl: state.configs.serverUrl,
 		isOnPrem: state.configs.isOnPrem,
-		offline: state.connectivity.offline
+		offline: state.connectivity.offline,
+		acceptedTOS: state.session.userId ? state.preferences.acceptedTOS : state.session.acceptedTOS
 	};
 };
 
@@ -143,6 +146,7 @@ const Root = connect(mapStateToProps)(props => {
 				</p>
 			</RoadBlock>
 		);
+	// if (!props.acceptedTOS) return <PresentTOS />;
 	if (!props.loggedIn) return <UnauthenticatedRoutes />;
 	if (props.company && props.company.plan === "TRIALEXPIRED") {
 		const upgradeLink = `${props.serverUrl}/web/subscription/upgrade/${props.company.id}`;
